ab934094d3c415e2f01e028cf5e8ad3b324abeb6
VOJABENGERLT\vojin <vojingalic03@gmail.com>
2026-02-16 13:51:56 +0100

    User service created to test out all the newly added devops structures

---
+++ services/user-service/src/db/database.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool.on('connect', () => {
+  console.log('Connected to PostgreSQL');
+});
+
+pool.on('error', (err) => {
+  console.error('PostgreSQL error:', err);
+});
+
+module.exports = pool;
